30d5fd8d903709507e13f215c526f3a5a986770b
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-03-21 16:18:10 -0300

    Configura conexão com MongoDB

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB conectado!'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+
+module.exports = mongoose;
